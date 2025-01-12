ffd86ff09bd7cb8823326c4030d1e7732f4eb902
ShamiKantMourya <shamimourya9852@gmail.com>
2025-01-12 19:15:24 +0530

    code refactor

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Header from '../components/Header';
-import JobCategories from '../components/JobCategories';
-import FeaturedJob from '../components/FeaturedJob';
-import BlogContainer from '../components/BlogContainer';
-import ClientBox from '../components/ClientBox';
-import ResumeBox from '../components/ResumeBox';
-import Footer from '../components/Footer';
+import React from "react";
+import Header from "../components/Header";
+import JobCategories from "../components/JobCategories";
+import FeaturedJob from "../components/FeaturedJob";
+import BlogContainer from "../components/BlogContainer";
+import Testimonials from "../components/Testimonials";
+import ResumeBox from "../components/ResumeBox";
+import Footer from "../components/Footer";
 
 const Homepage: React.FC = () => {
   return (
-    <div>
-        <Header />
-        <JobCategories />
-        <FeaturedJob />
-        <BlogContainer />
-        <ClientBox />
-        <ResumeBox />
-        <Footer />
-    </div>
-  )
-}
+    <main className="min-h-screen scroll-smooth">
+      <Header />
+      <JobCategories />
+      <FeaturedJob />
+      <BlogContainer />
+      <Testimonials />
+      <ResumeBox />
+      <Footer />
+    </main>
+  );
+};
 
-export default Homepage
+export default Homepage;
