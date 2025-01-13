4e4197397e488c2a5e6c0fca5f122a0378632630
ShamiKantMourya <shamimourya9852@gmail.com>
2025-01-13 13:32:39 +0530

    Job categories component

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Header from "../components/Header";
 import JobCategories from "../components/JobCategories";
-import FeaturedJob from "../components/FeaturedJob";
-import BlogContainer from "../components/BlogContainer";
-import Testimonials from "../components/Testimonials";
-import ResumeBox from "../components/ResumeBox";
-import Footer from "../components/Footer";
+// import FeaturedJob from "../components/FeaturedJob";
+// import BlogContainer from "../components/BlogContainer";
+// import Testimonials from "../components/Testimonials";
+// import ResumeBox from "../components/ResumeBox";
+// import Footer from "../components/Footer";
 
 const Homepage: React.FC = () => {
   return (
     <main className="min-h-screen scroll-smooth">
       <Header />
       <JobCategories />
-      <FeaturedJob />
-      <BlogContainer />
-      <Testimonials />
-      <ResumeBox />
-      <Footer />
+      {/* <FeaturedJob /> */}
+      {/* <BlogContainer /> */}
+      {/* <Testimonials /> */}
+      {/* <ResumeBox /> */}
+      {/* <Footer /> */}
     </main>
   );
 };
