f7f0d8696e4f08c2c6419d7b232d4f9d773d6a4a
ShamiKantMourya <shamimourya9852@gmail.com>
2025-01-13 13:32:52 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Add `html` if needed
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
