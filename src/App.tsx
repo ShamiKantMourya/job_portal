14b353a05a90887b6bf892c730df4bbaa08dbfae
ShamiKantMourya <shamimourya9852@gmail.com>
2025-01-18 12:08:12 +0530

    signup page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Homepage = lazy(() => import("./Pages/Homepage"));
+const SignUp = lazy(() => import("./components/SignUp"));
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
